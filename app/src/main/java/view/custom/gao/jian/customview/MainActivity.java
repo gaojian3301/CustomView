cfb4cca68bc1e9db6c2c0d8362fd044497d66886
jiangao <jiangao@pateo.com.cn>
2019-03-05 10:09:45 +0800

    create project

---
+++ app/src/main/java/view/custom/gao/jian/customview/MainActivity.java
@@ -0,0 +1,13 @@
+package view.custom.gao.jian.customview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
